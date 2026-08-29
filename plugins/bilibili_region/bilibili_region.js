e57d73e8ed809a87b472fbed5b7896d37c4da74e
TomCatXue <3505164245@qq.com>
2026-08-29 11:29:06 +0800

    fix(bilibili_region): 补充 .plugin 文件，JS 文件去掉内联 Loon 配置

--- plugins/bilibili_region/bilibili_region.js
+++ plugins/bilibili_region/bilibili_region.js
@@ -6,12 +6,7 @@
 @Author: TomCatXue
 @Date: 2026-08-29
 
-===== Loon =====
-[MITM]
-hostname = app.bilibili.com
-
-[Script]
-http-request ^https?://app\.bilibili\.com/x/v2/channel/region/list script-path=https://raw.githubusercontent.com/TomCatXue/MyCookieCenter/refs/heads/main/plugins/bilibili_region/bilibili_region.js, requires-body=false, timeout=10
+Loon 配置见 bilibili_region.plugin
 ------------------------------------------
 */
 
